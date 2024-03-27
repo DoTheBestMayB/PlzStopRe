2321f9d05a35d103ce6cde96361b6c1406559fdf
DoDoBest <send1684@gmail.com>
2024-03-27 22:43:10 +0900

    build: kts로 migration

---
+++ data/build.gradle.kts
@@ -0,0 +1,100 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+}
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
+
+android {
+    namespace = "com.stop.data"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+
+        buildConfigField("String", "T_MAP_URL", "${properties["t_map_url"]}")
+        buildConfigField("String", "WS_BUS_URL", "${properties["ws_bus_url"]}")
+        buildConfigField("String", "APIS_URL", "${properties["apis_url"]}")
+        buildConfigField("String", "OPEN_API_SEOUL_URL", "${properties["open_api_seoul_url"]}")
+        buildConfigField("String", "T_MAP_APP_KEY", "${properties["t_map_app_key"]}")
+        buildConfigField("String", "SUBWAY_KEY", "${properties["subway_key"]}")
+        buildConfigField("String", "BUS_KEY", "${properties["bus_key"]}")
+        buildConfigField(
+            "String",
+            "SW_OPEN_API_SEOUL_URL",
+            "${properties["sw_open_api_seoul_url"]}"
+        )
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:hilt-compiler:2.48.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    // OkHttp3
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.moshi:moshi-adapters:1.14.0")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+
+    // xml parser
+    implementation("com.tickaroo.tikxml:annotation:0.8.13")
+    implementation("com.tickaroo.tikxml:core:0.8.13")
+    implementation("com.tickaroo.tikxml:retrofit-converter:0.8.13")
+    ksp("com.tickaroo.tikxml:processor:0.8.13")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+}
